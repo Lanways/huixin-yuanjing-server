a369bef89bea7deb06a5b2606c6847e60e293cb0
Lanways <mcbluedd@gmail.com>
2024-05-08 20:57:27 +0800

    feat: import getEnvVariable

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,12 @@
 import express from "express";
 import dotenv from 'dotenv'
+import { getEnvVariable } from "./helpers/checkType";
 
 dotenv.config()
 const app = express()
-const PORT = process.env.PORT || 8000
-
+const PORT = getEnvVariable('PORT')
+const ENV = getEnvVariable('NODE_ENV')
+console.log('env is', ENV)
 app.get('/', (req, res, next) => {
   res.send('hello')
 })
