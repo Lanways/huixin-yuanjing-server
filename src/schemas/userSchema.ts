0228420e39f67a5b0e23e966787c77a37acdc58d
Lanways <mcbluedd@gmail.com>
2024-05-09 13:15:33 +0800

    feat: create user schema

---
+++ src/schemas/userSchema.ts
@@ -0,0 +1,13 @@
+import Joi from "joi"
+
+export const baseUserSchema = Joi.object({
+  name: Joi.string().min(3).max(50).allow(null).optional(),
+  account: Joi.string().allow(null).optional(),
+  address: Joi.string().allow(null).optional(),
+})
+
+export const userSchema = baseUserSchema.keys({
+  phone: Joi.string().required(),
+  email: Joi.string().email().required(),
+  password: Joi.string().min(8).max(20).required()
+})
